a083f6e00602999f31f63452bc3b707b4e711938
Yoan Chabot <yoan.chabot@orange.com>
2023-12-20 11:20:50 +0100

    Minor fix
    Signed-off-by: Yoan Chabot <yoan.chabot@orange.com>

--- annotation/annot_scripts/annotation_models.py
+++ annotation/annot_scripts/annotation_models.py
@@ -16,7 +16,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-"""import numpy as np
+"""
+import numpy as np
 import time
 import math
 import operator
